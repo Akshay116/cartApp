fd1a1af4807fb3ee369db21076360ff005f35ebf
akshay padmane <akshaypadmane@akshays-MacBook-Air.local>
2021-11-27 23:24:16 +0530

    addd increasequantity func

--- src/CartItem.js
+++ src/CartItem.js
@@ -7,11 +7,18 @@ class CartItem extends React.Component{
        super();
        this.state ={
            price:10000,
-           title:'Phone',
+           title:' Mobile Phone',
            qty:1,
            img:''
        }
+      // this.increaseQuantity =this.increaseQuantity.bind(this);
    }
+   
+     increaseQuantity = () =>{
+         console.log('this',this.state);//arrow function binding 
+     }
+
+
      render(){
          const {price,title,qty} = this.state;//object destructring
          return(
@@ -25,7 +32,11 @@ class CartItem extends React.Component{
                      <div style={{ color:'green'}}>Qty:{qty}</div>
                       <div className="cart-item-actions">
                           {/*buttons */}
-                          <img src="https://cdn-icons.flaticon.com/png/512/2740/premium/2740600.png?token=exp=1637932921~hmac=2822d17220995a194f970b2d53011ab6" alt="increase"  className="action-icons"/>
+                          <img src="https://cdn-icons.flaticon.com/png/512/2740/premium/2740600.png?token=exp=1637932921~hmac=2822d17220995a194f970b2d53011ab6"
+                            alt="increase" 
+                            className="action-icons"
+                            onClick = {this.increaseQuantity}
+                            />
                           <img src="https://cdn-icons-png.flaticon.com/512/992/992683.png" alt="decrease"  className="action-icons"/>
                           <img src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png" alt="delete"  className="action-icons"/>
                       </div>
